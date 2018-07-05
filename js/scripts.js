a4736fe30551831fb6b654b556dc89ce19ffc973
Tom Chege <emdeechege@gmail.com>
2018-07-05 04:55:02 +0300

    added some effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
 });
+$(document).ready(function() {
+  $("button").click(function() {
+    $("#div1").fadeIn();
+    $("#div2").fadeIn("slow");
+    $("#div3").fadeIn(3000);
+  });
+});
+$(document).ready(function() {
+  $("button").click(function() {
+    $("#div4").fadeOut();
+    $("#div5").fadeOut("slow");
+    $("#div6").fadeOut(3000);
+      });
+    });
+$(document).ready(function() {
+  $("#flip").click(function() {
+  $("#panel").slideDown("slow");
+    });
+      });
